9c07053e6c52dfe2d2fc40bf8421aecb1f58e029
sachinjose <sachinjose16@gmail.com>
2021-02-17 23:39:43 +0530

    Binary Search

---
+++ Binary Search/binary_search.cpp
@@ -0,0 +1,47 @@
+#include"/Users/sachinjose/Desktop/DSnA/stdc++.h"
+
+using namespace std;
+
+void binary_search(int array[],int n, int key){
+
+	int left = 0;
+	int right = n-1;
+	int mid = (left+right)/2;
+	while(left<=right){
+
+		mid = (left+right)/2;
+		if(array[mid] == key){
+			cout<<"Found at index: "<<mid<<endl;
+			return;
+		}
+		else if(array[mid]>key){ // value is less than middle value
+			right = mid - 1;
+		}
+		else {	//value is greater than middle value
+			left = mid + 1;
+		}
+	}
+
+	cout<<"Element is not in the list"<<endl;
+}
+
+int main(){
+
+	int n,key;
+	int flag = 0;
+
+	cin>>n;
+
+	int array[n];
+
+	for (int i = 0; i <= n-1; i++){
+		cin>>array[i];
+	}
+
+	cout<<"Enter Key"<<endl;
+	cin>>key;
+
+	binary_search(array,n,key);
+
+	return 0;
+}
